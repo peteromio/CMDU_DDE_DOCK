a9f0ff4add82e4b9dfdeda2f664c9e94aea824b0
Pedro Rodriguez <rpeter2047@outlook.com>
2020-09-23 00:15:17 -0400

    fix(build.sh) create the folder structure

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@ factory_fn()
     cd build
     cmake ..
     make
-    cp libCMDU.so ../pkg/usr/lib/dde-dock/plugins/
-    cd ../
-    dpkg -b ./pkg ./cmdu-dock-plugin_3.20.0-0_amd64.deb  
+    if [ -d "../pkg/usr/lib/dde-dock/plugins/" ]
+    then 
+        cp libCMDU.so ../pkg/usr/lib/dde-dock/plugins/
+        cd ../
+        dpkg -b ./pkg ./cmdu-dock-plugin_3.20.0-0_amd64.deb
+    else
+        mkdir -p ../pkg/usr/lib/dde-dock/plugins/
+        cp libCMDU.so ../pkg/usr/lib/dde-dock/plugins/
+        cd ../
+        dpkg -b ./pkg ./cmdu-dock-plugin_3.20.0-0_amd64.deb
+    fi
 }
 if [ -d "./build" ] 
 then
